14fdc262a5096b204db77da7fb1cb1f58751f802
root <root@DESKTOP-O5GBJ36.localdomain>
2022-06-06 20:06:22 +0100

    test: add test to address value object

--- src/entities/custumer.spec.ts
+++ src/entities/custumer.spec.ts
@@ -1,20 +1,21 @@
+import Address from './address'
 import Customer from './customer'
 
 describe('Customer bussines rules', () => {
 
-    it("should throw error on create a customer without id", () => {
+    test("should throw error on create a customer without id", () => {
         expect(() => {
             const customer = new Customer("", "Bento")
         }).toThrowError("Invalid id")
     })
 
-    it("should throw error on create a customer without name", () => {
+    test("should throw error on create a customer without name", () => {
         expect(() => {
             const customer = new Customer("123", "")
         }).toThrowError("Invalid name")
     })
 
-    it('should change name', () => {
+    test('should change name', () => {
 
         const costumer = new Customer("123", "Bento")
 
@@ -23,14 +24,14 @@ describe('Customer bussines rules', () => {
         expect(costumer.name).toBe("Siala")
     })
 
-    it('should throw error on change name to empty', () => {
+    test('should throw error on change name to empty', () => {
         expect(() => {
             const costumer = new Customer("123", "Bento")
             costumer.changeName("")
         }).toThrowError("Invalid name")
     })
 
-    it('should activate customer', () => {
+    test('should activate customer', () => {
         const customer = new Customer("123", "Bento")
 
         customer.activate()
@@ -38,11 +39,47 @@ describe('Customer bussines rules', () => {
         expect(customer.isActive()).toBe(true)
     })
 
-    it('should deactivate customer', () => {
+    test('should deactivate customer', () => {
         const customer = new Customer("123", "Bento")
 
         customer.deactivate()
 
         expect(customer.isActive()).toBe(false)
+    })
+
+})
+
+describe('Address Value Objects', () => {
+    test('should be able to create an address', () => {
+        const address = new Address("Cacuaco", 123, "000", "Luanda")
+
+        expect(address.street).toBe("Cacuaco")
+        expect(address.number).toBe(123)
+        expect(address.zip).toBe("000")
+        expect(address.city).toBe("Luanda")
+    })
+
+    test('should thrown an error on invalid street', () => {
+        expect(() => {
+            const address = new Address("", 123, "000", "Luanda")
+        }).toThrowError("Street is required")
+    })
+
+    test('should thrown an error on invalid number', () => {
+        expect(() => {
+            const address = new Address("Cacuaco", 0, "000", "Luanda")
+        }).toThrowError("Number is required")
+    })
+
+    test('should thrown an error on invalid number', () => {
+        expect(() => {
+            const address = new Address("Cacuaco", 123, "", "Luanda")
+        }).toThrowError("Zip is required")
+    })
+
+    test('should thrown an error on invalid city', () => {
+        expect(() => {
+            const address = new Address("Cacuaco", 123, "000", "")
+        }).toThrowError("City is required")
     })
 })
