0e20ee6596b36974429b30e11b6fcc7f02ed10dc
root <root@DESKTOP-O5GBJ36.localdomain>
2022-06-06 20:05:44 +0100

    feat: add address value object

--- src/entities/customer.ts
+++ src/entities/customer.ts
@@ -1,4 +1,7 @@
+import Address from './address'
 export default class Customer {
+
+    private _address: Address
 
     constructor(
         private _id: string,
@@ -19,6 +22,10 @@ export default class Customer {
 
     get name(): string {
         return this._name;
+    }
+
+    set address(address: Address) {
+        this._address = address
     }
 
     public changeName(name: string) {
