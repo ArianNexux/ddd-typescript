7e59e2b9d063bd2142ee153d1818576bc6179d40
ArianNexux <ariannexux0101@gmail.com>
2022-07-21 17:12:24 +0100

    chore: enable decorators to swc

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    ".(ts)": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+    ".(ts)": "@swc/jest"
   },
   testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
   moduleFileExtensions: ["ts", "tsx", "js"]
