fff3deaf1d116e367e06dcca63a3fd83a70fd942
ArianNexux <ariannexux0101@gmail.com>
2022-07-21 16:49:29 +0100

    feat: implement find and findAll products

--- src/infrastructure/repository/customer.repository.ts
+++ src/infrastructure/repository/customer.repository.ts
@@ -1,3 +1,4 @@
+import Address from "../../domain/entities/address";
 import Customer from "../../domain/entities/customer";
 import CustomerRepositoryInterface from "../../domain/repository/customer-repository.interface";
 import CustomerModel from "../db/sequelize/model/customer.model";
@@ -33,8 +34,23 @@ export default class CustomerRepository implements CustomerRepositoryInterface {
             }
         })
     }
-    find(id: string): Promise<Customer> {
-        throw new Error("");
+
+    async find(id: string): Promise<Customer> {
+        let customerModel
+        try {
+            customerModel = await CustomerModel.findOne({ where: { id: id }, rejectOnEmpty: true })
+        } catch (e) {
+            throw new Error("Customer is required");
+        }
+
+        const customer = new Customer(customerModel.id, customerModel.name)
+        const address = new Address(customerModel.street, customerModel.number, customerModel.zipcode, customerModel.city)
+        customer.changeAddress(address)
+        customer.addRewardPoint(customerModel.rewardPoints)
+
+        return customer
+
+
 
     }
 
