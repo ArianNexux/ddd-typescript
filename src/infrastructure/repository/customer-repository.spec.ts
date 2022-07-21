a59413f1a353862e0ec9e45d3f0b1d431605deef
ArianNexux <ariannexux0101@gmail.com>
2022-07-21 16:49:00 +0100

    test: find and findAll customers

--- src/infrastructure/repository/customer-repository.spec.ts
+++ src/infrastructure/repository/customer-repository.spec.ts
@@ -66,6 +66,61 @@ describe('Customer Repository', () => {
         })
     })
 
+    test('should find a customer given id', async () => {
+        const customer = new Customer("123", "customer 1", false)
+        const address = new Address("street 1", 1, "zipcode 1", "city 1")
+        customer.changeAddress(address)
+
+        const customerRepository = new CustomerRepository()
+        await customerRepository.create(customer)
+
+        const customerModel = await CustomerModel.findOne({ where: { id: customer.id } })
+
+        expect(customerModel.toJSON()).toStrictEqual({
+            id: customer.id,
+            name: customer.name,
+            city: customer.address.city,
+            zipcode: customer.address.zip,
+            number: customer.address.number,
+            street: customer.address.street,
+            rewardPoints: customer.reward_point
+        })
+    })
+
+    test('should find all customers', async () => {
+        const customer = new Customer("123", "customer 1", false)
+        const customer1 = new Customer("124", "customer 2", false)
+        const address = new Address("street 1", 1, "zipcode 1", "city 1")
+        customer.changeAddress(address)
+        customer1.changeAddress(address)
+
+        const customerRepository = new CustomerRepository()
+        await customerRepository.create(customer)
+        await customerRepository.create(customer1)
+
+        const customerModels = await CustomerModel.findAll()
+
+        expect(customerModels.map(customer => customer.toJSON())).toStrictEqual([
+            {
+                id: customer.id,
+                name: customer.name,
+                city: customer.address.city,
+                zipcode: customer.address.zip,
+                number: customer.address.number,
+                street: customer.address.street,
+                rewardPoints: customer.reward_point
+            },
+            {
+                id: customer1.id,
+                name: customer1.name,
+                city: customer1.address.city,
+                zipcode: customer1.address.zip,
+                number: customer1.address.number,
+                street: customer1.address.street,
+                rewardPoints: customer1.reward_point
+
+            }])
+    })
     afterEach(async () => {
         sequelize.close()
     })
