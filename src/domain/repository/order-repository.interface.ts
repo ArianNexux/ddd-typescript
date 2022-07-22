386cf65404bc5000e22138d81d4c6af2117f1393
ArianNexux <ariannexux0101@gmail.com>
2022-07-22 15:16:30 +0100

    feat: create repository of order

---
+++ src/domain/repository/order-repository.interface.ts
@@ -0,0 +1,4 @@
+import OrderRepository from "../../infrastructure/repository/order.repository";
+import RepositoryInterface from "./repository-interface";
+
+export default interface OrderRepositoryInterface extends RepositoryInterface<OrderRepository> { }
