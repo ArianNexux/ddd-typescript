3ea353869b50d00774b7be56c074a9e330cce3ed
ArianNexux <ariannexux0101@gmail.com>
2022-07-25 17:23:17 +0100

    feat: implement create method

--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -1,16 +1,35 @@
+import Order from "../../domain/entities/order";
 import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
+import OrderItemModel from "../db/sequelize/model/order-item.model";
+import OrderModel from "../db/sequelize/model/order.model";
 
 export default class OrderRepository implements OrderRepositoryInterface {
-    create(entity: OrderRepository): Promise<void> {
+    async create(entity: Order): Promise<void> {
+        await OrderModel.create(
+            {
+                id: entity.id,
+                customer_id: entity.customerId,
+                total: entity.total(),
+                items: entity.items.map(item => ({
+                    id: item.id,
+                    name: item.name,
+                    quantity: item.quantity,
+                    price: item.price,
+                    product_id: item.productId
+                }
+                )),
+            },
+            {
+                include: [{ model: OrderItemModel }]
+            })
+    }
+    update(entity: Order): Promise<void> {
         throw new Error("Method not implemented.");
     }
-    update(entity: OrderRepository): Promise<void> {
+    find(id: string): Promise<Order> {
         throw new Error("Method not implemented.");
     }
-    find(id: string): Promise<OrderRepository> {
-        throw new Error("Method not implemented.");
-    }
-    findAll(): Promise<OrderRepository[]> {
+    findAll(): Promise<Order[]> {
         throw new Error("Method not implemented.");
     }
 
