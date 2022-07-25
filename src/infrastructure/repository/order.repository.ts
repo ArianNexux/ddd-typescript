4dc868be746b12a934323ba4837881b7be372d23
ArianNexux <ariannexux0101@gmail.com>
2022-07-25 18:25:30 +0100

    feat: implement method to find one order

--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -1,4 +1,5 @@
 import Order from "../../domain/entities/order";
+import OrderItem from "../../domain/entities/order_item";
 import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
@@ -43,8 +44,29 @@ export default class OrderRepository implements OrderRepositoryInterface {
                 }
             })
     }
-    find(id: string): Promise<Order> {
-        throw new Error("Method not implemented.");
+    async find(id: string): Promise<Order> {
+        try {
+            const order = await OrderModel.findOne({
+                where: {
+                    id
+                },
+                include: [{ model: OrderItemModel }]
+            })
+
+            const orderItems = order.items.map(item => new OrderItem(
+                item.id,
+                item.name,
+                item.price,
+                item.product_id,
+                item.quantity
+            ))
+
+            return new Order(order.id, order.customer_id, orderItems);
+
+        }
+        catch (e) {
+            throw new Error("Order is required");
+        }
     }
     findAll(): Promise<Order[]> {
         throw new Error("Method not implemented.");
