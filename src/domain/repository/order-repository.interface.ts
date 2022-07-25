8312e43813b6918e768807b66072c30ebffc2337
ArianNexux <ariannexux0101@gmail.com>
2022-07-25 16:21:58 +0100

    fix: correct generic type

--- src/domain/repository/order-repository.interface.ts
+++ src/domain/repository/order-repository.interface.ts
@@ -1,5 +1,6 @@
-import { Order } from "sequelize/types";
+
 import OrderRepository from "../../infrastructure/repository/order.repository";
+import Order from "../entities/order";
 import RepositoryInterface from "./repository-interface";
 
 export default interface OrderRepositoryInterface extends RepositoryInterface<Order> { }
