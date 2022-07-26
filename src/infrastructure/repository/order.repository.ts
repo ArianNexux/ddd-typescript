fb4a2cdb7dfe3db0ca1eefe71d9a268aed658d18
ArianNexux <ariannexux0101@gmail.com>
2022-07-26 14:12:22 +0100

    refactor: update order items

--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -25,24 +25,39 @@ export default class OrderRepository implements OrderRepositoryInterface {
             })
     }
     async update(entity: Order): Promise<void> {
-        await OrderModel.update(
-            {
-                id: entity.id,
-                customer_id: entity.customerId,
-                total: entity.total(),
-                items: entity.items.map(item => ({
-                    id: item.id,
-                    name: item.name,
-                    quantity: item.quantity,
-                    price: item.price,
-                    product_id: item.productId
-                })),
-            },
-            {
-                where: {
-                    id: entity.id
-                }
-            })
+        const productFound = await OrderModel.findOne({
+            where: {
+                id: entity.id
+            }
+        });
+        if (!productFound) {
+            throw new Error("Order not found");
+        }
+
+        const sequelize = OrderModel.sequelize;
+        await sequelize.transaction(async (t) => {
+            await OrderItemModel.destroy({
+                where: { order_id: entity.id },
+                transaction: t,
+            });
+            const items = entity.items.map((item) => ({
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                product_id: item.productId,
+                quantity: item.quantity,
+                order_id: entity.id,
+            }));
+
+            OrderItemModel.bulkCreate(items, { transaction: t });
+            await OrderModel.update(
+                {
+                    customer_id: entity.customerId,
+                    total: entity.total()
+                },
+                { where: { id: entity.id }, transaction: t }
+            );
+        });
     }
     async find(id: string): Promise<Order> {
         try {
