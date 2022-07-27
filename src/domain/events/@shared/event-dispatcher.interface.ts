6684e9bc48b1cc693f112738ec69d05ac1063b2f
ArianNexux <ariannexux0101@gmail.com>
2022-07-27 15:48:35 +0100

    feat: add event dispacther interface

--- src/domain/events/@shared/event-dispatcher.interface.ts
+++ src/domain/events/@shared/event-dispatcher.interface.ts
@@ -4,5 +4,6 @@ import EventHandlerInterface from "./event-handler.interface";
 export default interface EventDispatcherInterface {
     notify(event: EventInterface):void
     register(eventName: String, eventHandler: EventHandlerInterface): void
+    unregister(eventName: String, eventHandler: EventHandlerInterface)
     unregisterAll(): void
 }
