0f0a9e9d966ec83697f76ad5b4b1c669c47fd1bf
ArianNexux <ariannexux0101@gmail.com>
2022-07-28 16:30:14 +0100

    test: should notify all events

--- src/domain/events/@shared/event-dispatcher.spec.ts
+++ src/domain/events/@shared/event-dispatcher.spec.ts
@@ -1,5 +1,6 @@
 import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
 import EventDispatcher from "./event-dispatcher";
+import ProductCreatedEvent from "../product/product-created.event";
 
 describe("Event Tests", ()=>{
     test("should register an event handler", ()=>{
@@ -37,6 +38,23 @@ describe("Event Tests", ()=>{
         eventDispatcher.unregisterAll()
 
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined()
+    })
+
+    test("should notify all event handlers", ()=>{
+        const eventDispatcher = new EventDispatcher()
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler()
+        const spyHandler = jest.spyOn(eventHandler, "handle")
+        eventDispatcher.register("ProductCreatedEvent", eventHandler)
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler)
+        const productCreatedEvent = new ProductCreatedEvent({
+            name: "Product 1",
+            price: 10.0,
+            description: "such product",
+        })
+        eventDispatcher.notify(productCreatedEvent)
+
+        expect(spyHandler).toHaveBeenCalled()
     })
 
 
