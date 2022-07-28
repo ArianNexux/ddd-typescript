8b9b383fa158d7b8f23e541c3d957da16a3d945f
ArianNexux <ariannexux0101@gmail.com>
2022-07-28 16:18:23 +0100

    refactor: take out dateTime from constructor

--- src/domain/events/product/product-created.event.ts
+++ src/domain/events/product/product-created.event.ts
@@ -1,9 +1,8 @@
 import EventInterface from "../@shared/event.interface";
 
 export default class ProductCreatedEvent implements EventInterface{
-
+    public dateTimeOcurred: Date = new Date()
     constructor(
-        public dateTimeOcurred: Date = new Date(),
         public eventData: any
     ){
 
