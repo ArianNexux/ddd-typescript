9762a1c9f1812e21570afd0ba8465744bb0b9274
Bento Julio <bento.julio@ima.gov.ao>
2023-01-05 14:01:43 +0100

    fix: add return type on execute method

--- src/usecase/product/find/find.product.usecase.ts
+++ src/usecase/product/find/find.product.usecase.ts
@@ -1,11 +1,11 @@
 import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
-import { InputFindProductUseCaseDTO } from "./find.product.dto";
+import { InputFindProductUseCaseDTO, OutputFindProductUseCaseDTO } from "./find.product.dto";
 
 export default class FindProductUseCase {
     constructor(private repository: ProductRepository) {
     }
 
-    async execute(input: InputFindProductUseCaseDTO) {
+    async execute(input: InputFindProductUseCaseDTO): Promise<OutputFindProductUseCaseDTO> {
         const result = await this.repository.find(input.id);
 
 
