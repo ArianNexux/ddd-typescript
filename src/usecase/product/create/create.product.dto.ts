ee784673b52ac4a467aa1934f6777cfde8fad404
Bento Julio <bento.julio@ima.gov.ao>
2023-01-05 10:37:39 +0100

    feat: implement dto of output and input to usecase of create product

---
+++ src/usecase/product/create/create.product.dto.ts
@@ -0,0 +1,10 @@
+export interface InputCreateProductUseCaseDTO {
+    name: string;
+    price: number;
+}
+
+export interface OutputCreateProductUseCaseDTO {
+    id: string;
+    name: string;
+    price: number;
+}
