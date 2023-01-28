a69cc6e9a9b4ae13bf4c04521cd9639ada28d1a4
Bento Julio <bento.julio@ima.gov.ao>
2023-01-28 19:33:05 +0100

    feat: add test for list all products

--- src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -32,6 +32,40 @@ describe('End to End Tests for products', () => {
         expect(createProduct.status).toBe(500)
     })
 
+    it("should list all products", async () => {
+        const product1 = {
+            name: "p1",
+            price: 100
+        }
+
+        const product2 = {
+            name: "p2",
+            price: 200
+        }
+
+        const createProduct1 = await request(app)
+            .post('/products')
+            .send(product1)
+
+        const createProduct2 = await request(app)
+            .post('/products')
+            .send(product2)
+
+        expect(createProduct1.status).toBe(200)
+        expect(createProduct2.status).toBe(200)
+
+        const responseListProduct1 = await request(app).
+            get('/products').
+            send()
+
+        expect(responseListProduct1.status).toBe(200)
+        expect(responseListProduct1.body.products[0].name).toBe(product1.name)
+        expect(responseListProduct1.body.products[1].name).toBe(product2.name)
+        expect(responseListProduct1.body.products[0].price).toBe(product1.price)
+        expect(responseListProduct1.body.products[1].price).toBe(product2.price)
+
+    })
+
     afterAll(async () => {
         await sequelize.close()
     })
