69cf0bd324d78f5346faff6f436052c738d826be
Bento Julio <bento.julio@ima.gov.ao>
2023-01-28 19:33:30 +0100

    feat: add route to list all products

--- src/infrastructure/api/routes/product.route.ts
+++ src/infrastructure/api/routes/product.route.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express'
 import CreateProductUseCase from '../../../usecase/product/create/create.product.usecase';
+import ListProductUseCase from '../../../usecase/product/list/list.product.usecase';
 import ProductRepository from '../../product/repository/sequelize/product.repository';
 
 
@@ -21,4 +22,16 @@ productRoute.post('/', async (req: Request, res: Response) => {
     } catch (err) {
         res.status(500).send(err.message)
     }
+})
+
+productRoute.get('/', async (req: Request, res: Response) => {
+    const usecase = new ListProductUseCase(new ProductRepository())
+
+    try {
+        const output = await usecase.execute({})
+        res.status(200).send(output)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
 })
