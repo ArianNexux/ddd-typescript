73b12f5b44204800e9526e18fd7be76f4622e1b5
ArianNexux <ariannexux0101@gmail.com>
2023-01-29 23:41:03 +0100

    feat: add props of notification error and implement class of noification

---
+++ src/domain/@shared/notification/notification.ts
@@ -0,0 +1,27 @@
+export type NotificationErrorProps = {
+    message: string
+    context: string
+}
+
+export class Notification {
+    private error: NotificationErrorProps[]
+    constructor() {
+        this.error = [];
+    }
+    addError(message: NotificationErrorProps) {
+        this.error.push(message)
+    }
+
+    messages(context: string) {
+        const output = [];
+        this.error.map(notification => {
+            if (context === notification.context) {
+                output.push(`${notification.context}: ${notification.message}`)
+            }
+        })
+
+        return output.join(',')
+    }
+
+
+}
