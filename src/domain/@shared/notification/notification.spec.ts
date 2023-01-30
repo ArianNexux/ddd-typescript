ad9de65e548cb6436f273b0b3b220589102a6e35
ArianNexux <ariannexux0101@gmail.com>
2023-01-30 10:31:04 +0100

    test: should get all errors props

--- src/domain/@shared/notification/notification.spec.ts
+++ src/domain/@shared/notification/notification.spec.ts
@@ -27,4 +27,30 @@ describe('Test suits of notifications', () => {
         notification.addError(error3);
         expect(notification.messages()).toBe("customer: error message, customer: error message2, order: error message3");
     })
+
+    it('should check if a notification has at least one error', () => {
+        const notification = new Notification();
+        const error = {
+            message: "error message",
+            context: "customer"
+        }
+
+        notification.addError(error);
+
+        expect(notification.hasErrors()).toBe(true)
+    })
+
+    it('should get all errors props', () => {
+
+        const notification = new Notification();
+        const error = {
+            message: "error message",
+            context: "customer"
+        }
+
+        notification.addError(error);
+
+        expect(notification.getErrors()).toEqual([error])
+
+    })
 })
