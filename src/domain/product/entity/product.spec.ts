ec0c5966a7986a696d3feeef0db8a2a5b8575cd4
ArianNexux <ariannexux0101@gmail.com>
2023-02-07 11:13:00 +0100

    test: when id, name, price is invalid

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,4 +1,3 @@
-import Order from "./order"
 import Product from "./product"
 
 describe('Product unit test', () => {
@@ -8,20 +7,26 @@ describe('Product unit test', () => {
 
             let product = new Product("", "product 1", 100)
 
-        }).toThrowError("Id is required")
+        }).toThrowError("product: Id is required")
     })
 
     test('should throw an error if price is less or equal then zero', () => {
         expect(() => {
             let product = new Product("123", "product 1", 0)
-        }).toThrowError("Invalid price provided")
+        }).toThrowError("product: Invalid price provided")
 
     })
 
     test('should throw an error if no name is provided', () => {
         expect(() => {
+            let product = new Product("", "", 0)
+        }).toThrowError("product: Name is required")
+    })
+
+    test('should throw an error if no name, price and id was provided is provided', () => {
+        expect(() => {
             let product = new Product("123", "", 10)
-        }).toThrowError("Name is required")
+        }).toThrowError("product: Id is required, product: Name is required, product: Invalid price provided")
     })
 
 
